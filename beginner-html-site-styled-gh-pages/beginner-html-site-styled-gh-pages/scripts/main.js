0e268248681404b7b45c81d35f60221b54d943c5
Foxei-i <foxeii177@gmail.com>
2025-03-21 09:38:13 -0700

    Update main.js

--- beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -23,8 +23,8 @@ const myImage = document.querySelector("img");
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/images/firefox2.png");
+    myImage.setAttribute("src", "images/firefox2.png");
   } else {
-    myImage.setAttribute("src", "beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/images/firefox-icon.png");
+    myImage.setAttribute("src", "images/firefox-icon.png");
   }
 });
