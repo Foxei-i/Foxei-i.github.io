3da3bf5e7d539a32ec1d270a80475ea9a4e47d75
Foxei-i <foxeii177@gmail.com>
2025-03-21 09:33:42 -0700

    Update main.js

--- beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,5 +1,5 @@
 let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+const myHeading = document.querySelector("h1");
 
 function setUserName() {
   const myName = prompt("Please enter your name.");
